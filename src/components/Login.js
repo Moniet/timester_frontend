04ce010c50aba9a67b44e6ce026814f6f0fcdef2
moniet <moniet.sawhney@gmail.com>
2019-08-08 23:12:10 +0100

    added name input for register page to dynamically render.

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,11 +12,7 @@ const Container = styled('div')`
     align-items: center;
     width: 100%;
     height: 100%;
-    background: linear-gradient(to top, 
-        ${colors.primary}, 
-        ${colors.secondary}, 
-        ${colors.tertiary}
-    );
+    background: linear-gradient(to top, ${colors.primary}, ${colors.secondary}, ${colors.tertiary});
 `
 
 const Form = styled('form')`
@@ -40,7 +36,7 @@ const Form = styled('form')`
         font-weight: 500;
         color: white;
         margin-bottom: 1em;
-        font-size: 1em;
+        font-size: 1rem;
     }
 
     input {
@@ -52,61 +48,74 @@ const Form = styled('form')`
         margin-bottom: 2rem;
         box-shadow: ${styles.shadow};
         letter-spacing: 2px;
-        font-size: 1em;
+        font-size: 0.75em;
     }
 
-    a {
+    span {
         color: white;
         align-self: flex-end;
         font-size: 0.75rem;
         margin-top: -1rem;
+        cursor: pointer;
     }
 `
 
-const Button = styled('div')`
-    padding: 1em;
+const Button = styled('button')`
+    padding: 0.9rem;
     font-weight: bold;
+    font-size: 0.75rem;
     border: none;
     background: ${colors.button};
     border-radius: 10px;
-    margin-top: 2em;
-    ${mq[0]} { 
-        width: 50%; 
-    }
-    ${mq[1]} { 
-        width: 50%; 
-    }
+    margin-top: 2rem;
+    color: white;
+    cursor: pointer;
+    ${mq[0]} {  width: 50%; }
+    ${mq[1]} { width: 50%; }
 `
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [registerPage, setRegisterPage] = useState(false)
 
     const handleUsernameChange = e => {
-        setUsername(e.target.value);
+        setUsername(e.target.value)
     }
 
     const handlePasswordChange = e => {
-        setPassword(e.target.value);
+        setPassword(e.target.value)
+    }
+
+    const handleClick = () => {
+        setRegisterPage(true)
+    }
+
+    const nameInput = () => {
+        if (registerPage) { 
+           return ( 
+                <>
+                    <label>name</label> 
+                    <input type='text' data-type='name' onChange={(e) => handleUsernameChange(e)} /> 
+                </>
+            )
+        }
     }
 
     return (
         <Container>
             <img src={require('../assets/img/form-memphis-design-top.svg')} />
             <Form role="form">
-                <label>Username</label>
-                <input 
-                    type='text' 
-                    data-type='username'
-                    onChange={(e) => handleUsernameChange(e)}
-                />
+                { nameInput() }
                 <label>Password</label>
-                <input 
-                    type='password' 
-                    date-type='password'
-                    onChange={(e) => handlePasswordChange(e)}
-                />
-                <Link to="/register">Register</Link>
+                <input type='text' data-type='username' onChange={(e) => handleUsernameChange(e)} />
+
+                <label>Password</label>
+                <input type='password' date-type='password' onChange={(e) => handlePasswordChange(e)} />
+
+                <span onClick={() => handleClick()}>Register</span>
                 <Button>Login</Button>
             </Form>
             <img src={require('../assets/img/form-memphis-design-bottom.svg')} />
